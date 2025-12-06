73749d8e39daef9e4d143dddeecb85480f234e86
Isaac-Morgan <morganisaackojo5547@gmail.com>
2025-12-06 00:59:10 +0000

    RescueLink

---
+++ index.js
@@ -0,0 +1,20 @@
+let later = document.getElementById("later")
+let coming = document.getElementById("coming")
+let save = document.getElementById("save")
+let setting = document.getElementById("setting")
+
+later.addEventListener("click", () => {
+    alert("Nothing here for now")
+})
+
+coming.addEventListener("click", () => {
+    alert("Coming soon")
+})
+
+save.addEventListener("click", () => {
+    alert("You have successfully saved a contact")
+})
+
+setting.addEventListener("click", () => {
+    alert("Settings saved")
+})
